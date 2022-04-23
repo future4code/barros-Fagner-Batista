1093759838ed99850a8996982cbe96cceeaa0db0
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2022-04-22 22:36:54 -0300

    Entrega de Exercicios do projeto

---
+++ modulo1/projeto-lista-js/exercicios.js
@@ -0,0 +1,189 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+  // implemente sua lógica aqui
+
+let altura = Number(prompt("Digite a altura"))
+let largura = Number(prompt("Digite a largura"))
+
+
+
+ console.log(altura * largura)
+}
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+  // implemente sua lógica aqui
+let anoAtual = Number(prompt("Qual o ano atual?"))
+let anoNasc = Number(prompt ("Qual o ano que você nasceu?"))
+
+console.log(anoAtual - anoNasc)
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+  // implemente sua lógica aqui
+
+ imc = peso /(altura * altura) 
+
+return imc
+
+}
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  //"Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+
+const nome = prompt("Qual o seu nome?")
+const idade = prompt("Quantos anos você tem?")
+const email = prompt("Qual o seu mail?")
+
+
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+
+
+  const cor1 = prompt("Qual sua 1ªcor favorita")
+  const cor2 = prompt("Qual sua 2ª cor favorita")
+  const cor3 = prompt("Qual sua 2ª cor favorita")
+
+   let array = [cor1, cor2, cor3]
+ 
+
+  console.log([cor1, cor2, cor3])
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+
+return stringMaiuscula = string.toUpperCase()
+
+}
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+  quant = custo/valorIngresso
+
+  return quant
+
+}
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+ 
+
+   return string1.length == string2.length
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+
+return array[0]
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+return array[array.length-1]
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+  string1 = "oi"
+  string2 = "oi"
+
+ 
+
+
+  return string1 === string2
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
