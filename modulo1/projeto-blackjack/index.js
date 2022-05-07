81cac16b067d2ad31078b0d5ed28d091d2f5c84a
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2022-05-06 21:47:21 -0300

    Entrega do Projeto BlackJack

---
+++ modulo1/projeto-blackjack/index.js
@@ -0,0 +1,18 @@
+
+console.log("Bem vido ao Jogo de BackJack")
+
+confirm = prompt("Deseja iniciar uma nova rodada? ")
+
+confirm1 = "Sim"
+
+
+if(confirm === confirm1) {
+{
+  console.log("Vamos começar")
+
+}
+
+
+}else if(confirm != confirm1){
+  console.log("O jogo acabou")
+}
