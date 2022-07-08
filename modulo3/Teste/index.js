0ace69b503438f5d68406a6840fb13cc8a6ad523
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2022-07-08 18:37:40 -0300

    testando

---
+++ modulo3/Teste/index.js
@@ -0,0 +1,14 @@
+
+
+console.log("===========================")
+const num1 = prompt("Digite o primeiro")
+const num2 = prompt("Digite o segundo")
+const soma = parseInt(num1)+parseInt(num2) 
+
+console.log(number = soma)
+
+console.log("===========================")
+const num3 = 10
+const num4 = 17
+const soma2 = num3 * num4
+console.log(soma2)
