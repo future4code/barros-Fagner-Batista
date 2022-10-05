aa65617d137df11e67d37b5b9804017f314d21fe
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2022-10-04 22:15:50 -0300

    Entrega revisão 04/10

---
+++ modulo5/revisao-javascript-backend/exercicio1/index.js
@@ -0,0 +1,17 @@
+//Este programa cria um conversor de temperaturas de celcius para as escalas em Fahrenheit e Kelvin. 
+const converter = {
+    kelvin: ()=>{
+        tempKelvin = 15 * 274,15
+        console.log(" A conversão de celsius para kelvin é de " + tempKelvin)
+    },
+
+    fahrenheit: ()=>{
+        tempFahrenheit = 15 *33,8
+        console.log(" A conversão de celsius para fahrenheit é de " + tempFahrenheit)
+    }
+
+
+}
+
+converter.kelvin()
+converter.fahrenheit()
